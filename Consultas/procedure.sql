c548cd5ab9082905c294a328cdf226c0fcae16cb
jgiglio <jgiglio@scainventarios.cl>
2018-11-08 17:40:23 -0300

    added sql procedures

--- Consultas/procedure.sql
+++ Consultas/procedure.sql
@@ -1,28 +1,28 @@
-CREATE PROCEDURE `normalize_results_final` ()
+CREATE PROCEDURE `normalize_results_lr` ()
 BEGIN
 
-declare cur cursor for select dataset from parameters group by dataset;
+declare cur cursor for select id from parameters_lr group by id;
 
 open cur;
 read_loop:LOOP
 fetch cur into id_dataset;
-if done then 
+if done then
 leave read_loop;
 end if;
 
-set @xmax=(select max(x) from results_final where dataset=id_dataset);
-set @ymax =(select max(y) from results_final where dataset=id_dataset);
-  
-set @xmin=(select min(x) from results_final where dataset=id_dataset);
-set @ymin =(select min(y) from results_final where dataset=id_dataset);
-  
-  
-UPDATE results_final 
-SET 
+set @xmax=(select max(x) from results_lr where parameters_id=id_dataset);
+set @ymax =(select max(y) from results_lr where parameters_id=id_dataset);
+
+set @xmin=(select min(x) from results_lr where parameters_id=id_dataset);
+set @ymin =(select min(y) from results_lr where parameters_id=id_dataset);
+
+
+UPDATE results_lr
+SET
     xnorm = (x - @xmin) / (@xmax - @xmin),
     ynorm = (y - @ymin) / (@ymax - @ymin)
 WHERE
-    dataset = id_dataset;
+    parameters_id= id_dataset;
 end loop;
 close cur;
 END
